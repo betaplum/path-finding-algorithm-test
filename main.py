470b2e64d908bb4f3e7f1f5c1ea17ea5fa3944e8
Vuyisile Ndlovu <terrameijar@gmail.com>
2020-04-01 18:02:48 +0200

    Add solution

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ col = 5
 
 
 def isPath(arr):
-
-    # Please write your code here
-
-    return True
-
+    path = False
+    paths = []
+    for index, i in enumerate(arr):
+        if i[index] == 0:
+            path = True
+            paths.append(0)
+        else:
+            path = False
+    if paths != [0, 0, 0, 0, 0]:
+        return False
+    else:
+        return True
 
 # Test data
 arr = [[0, 0, 0, -1, 0],
