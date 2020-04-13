ef6631a589e21cdbccf41ad618ba7b4941cc7cf5
JackieBinya <jacquelinebinya@gmail.com>
2020-04-13 10:29:12 +0200

    Edits main.js | Adds algorithm

--- main.js
+++ main.js
@@ -2,21 +2,33 @@ const row = 5;
 const col = 5;
 
 const isPath = (arr) => {
-  // Please write your code here
+ arr[0][0] = 1; 
+  
+ for (let i = 1; i < row; i++)  
+     if (arr[i][0] != -1) 
+         arr[i][0] = arr[i - 1][0];    
 
-  return true;
+ for (let j = 1; j < col; j++)  
+     if (arr[0][j] != -1) 
+         arr[0][j] = arr[0][j - 1];     
+
+ for (let i = 1; i < row; i++)  
+     for (let j = 1; j < col; j++)  
+       if (arr[i][j] != -1) 
+           arr[i][j] = Math.max(arr[i][j - 1], 
+                         arr[i - 1][j]);        
+   
+ return (arr[row - 1][col - 1] === 1)
 };
 
-// Test data
 const arr = [
   [0, 0, 0, -1, 0],
   [0, 0, 0, -1, -1],
   [0, 0, 0, -1, 0],
-  [-1, 0, -1, 0, -1],
+  [-1,0, -1, 0, -1],
   [0, 0, -1, 0, 0],
 ];
 
-// Main test
 if (isPath(arr)) {
   console.log("Yes");
 } else {
